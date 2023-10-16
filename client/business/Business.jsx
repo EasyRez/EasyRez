203280e3c697aed6e62659c4e40cadc8be407954
Akon530 <Akon530@users.noreply.github.com>
2023-10-16 17:49:55 -0400

    start building front-end

---
+++ client/business/Business.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Business = () => {
+
+    return (
+      <h1>This is Business</h1>
+    )
+};
+  
+export default Business;
---
+++ client/consumer/Consumer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Consumer = () => {
+
+    return (
+      <h1>This is Consumer</h1>
+    )
+};
+  
+export default Consumer;
