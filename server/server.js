39fedbdbcb23e2944b4ec9aae6b015424ffb9a95
danielkim-dk <danielkim.sung@gmail.com>
2023-10-16 14:55:48 -0700

    commit to update dev

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
+const path = require('path');
 const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get("/", (req, res) => {
+    res.send("hello world!!");
+})
+
+app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)});
---
+++ server/sql/connect.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+require('dotenv').config();
+
+let PG_URL = process.env.PG_URL;
+
+const pool = new Pool({
+    
+})
