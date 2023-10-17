f258837ef46ab97ce86b7768cdbf30257bf7496d
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-10-17 14:20:12 -0400

    fleshed out user model tests

--- __tests__/dbActions.js
+++ __tests__/dbActions.js
@@ -13,15 +13,26 @@ describe('SQL unit tests', () => {
     await createTables(); // make sure this exists in connect
   });
 
+  describe('User sign up model tests', () => {
+
+  })
+
 
   describe('Business model tests', () => {
+    let userId;
+
+    beforeAll(async () => {
+      const userInfo = { username: 'user', email: 'user@user.com', password: 'test', role: 'business' };
+      const user = User.createUser(userInfo);
+      userId = user.id;
+    })
 
     describe('create a business functionality', () => {
       let newBusinessId;
 
       it('should create a business with the correct properties', async () => {
-        const newBusinessInfo = { name: 'Test business' };
-        const newBusiness = await Business.createService(newBusinessInfo);
+        const newBusinessInfo = { userId, name: 'Test business' };
+        const newBusiness = await Business.createBusiness(newBusinessInfo);
         newBusinessId = newBusiness.id;
         expect(newBusiness.id).toBeDefined();
         expect(newBusiness.name).toEqual(newBusinessInfo.name);
@@ -89,42 +100,54 @@ describe('SQL unit tests', () => {
 
   describe('User model tests', () => {
     beforeAll(async () => {
+
+      // creating mock users
+      const consumerInfo = { username: 'user', email: 'user@user.com', password: 'test', role: 'consumer' };
+      const consumer = User.createUser(consumerInfo);
+      const consumerId = consumer.id;
+      const adminInfo = { username: 'admin', email: 'admin@admin.com', password: 'test', role: 'business' };
+      const admin = User.createUser(adminInfo);
+      const adminId = admin.id;
+
       // creating mock business
-      const businessInfo = { name: 'Cat Salon' };
+      const businessInfo = { userId: adminId, name: 'Cat Salon' };
       const business = await Business.createService(businessInfo);
-      businessId = business.id;
+      const businessId = business.id;
 
       // creating mock services
       const service1Info = { businessId, name: 'Whisker and nail trim', price: 50, duration: 60 };
       const service1 = await Business.createService(businessInfo);
-      service1Id = service1.id;
+      const service1Id = service1.id;
       const service2Info = { businessId, name: 'Paw rub', price: 25, duration: 30 };
       const service2 = await Business.createService(businessInfo);
-      service2Id = service2.id;
+      const service2Id = service2.id;
 
       // creating mock timeslots
-      const timeslot1Info = { serviceId, startTime: '2023-10-31 10: 30: 00' };
+      const timeslot1Info = { service1Id, startTime: '2023-10-31 10: 30: 00' };
       const timeslot1 = await Business.createTimeslot(timeslot1Info);
-      timeslot1Id = timeslot1.id;
-      const timeslot2Info = { serviceId, startTime: '2023-10-31 11: 30: 00' };
+      const timeslot1Id = timeslot1.id;
+      const timeslot2Info = { service1Id, startTime: '2023-10-31 11: 30: 00' };
       const timeslot2 = await Business.createTimeslot(timeslot2Info);
-      timeslot2Id = timeslot2.id;
+      const timeslot2Id = timeslot2.id;
     })
 
     describe('get all services functionality', () => {
       it('should get all services for a given business', async () => {
-
+        const services = await User.getServices({ businessId });
+        expect(services.length).toEqual(2);
       })
     })
 
     describe('get all time slots functionality', () => {
       it('should get all time slots for a given service', async () => {
-
+        const services = await User.getTimeslots({ service1Id });
+        expect(services.length).toEqual(2);
       })
     })
 
     describe('book a reservation', () => {
       it('should book a new reservation', async () => {
+        const reservation = await User.createReservation( {})
 
       })
     })
